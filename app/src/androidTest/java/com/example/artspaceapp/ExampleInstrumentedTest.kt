82946dab194665051c3138f6e40d72029e6a6c0c
pierre.gabriel@sportypeppers.com <pierre.gabriel@sportypeppers.com>
2023-01-04 18:35:57 -0600

    readyToMerge

--- app/src/androidTest/java/com/example/artspaceapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/artspaceapp/ExampleInstrumentedTest.kt
@@ -1,12 +1,17 @@
 package com.example.artspaceapp
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.artspaceapp.ui.theme.ArtSpaceAppTheme
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +19,17 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class   ExampleInstrumentedTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.artspaceapp", appContext.packageName)
+    fun InstrumentedTest() {
+        composeTestRule.setContent {
+            ArtSpaceAppTheme {
+                RenderArt()
+            }
+        }
+
     }
 }
